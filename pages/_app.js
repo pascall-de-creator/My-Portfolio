d426f443b0e3f134636d945d5805cacb449fe922
pascall de creator <pascall.de.creator@gmail.com>
2022-05-01 06:43:06 -0700

    Finish Home page. initial version 1.0.0

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Pascal Nkornyui Developer Portfolio . Best Developer for frontend backend and fullstack developing . Pascall de creator . Pascall de developer ." />
-        <meta name="keywords" content="Portfolio , developer , best , frontend - front end development , backend - back end developing , full stack development , pascall de crreator , pascall de developer , pascal nkornyui" />
+        <meta name="keywords" content="Portfolio , developer , best , frontend - front end development , backend - back end developing , full stack development , pascall de crreator , pascall de developer , pascal nkornyui" /> 
       </Head>
       <div className='flex flex-col items-center'>
         <Component {...pageProps} />
