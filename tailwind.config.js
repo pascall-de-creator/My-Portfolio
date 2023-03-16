01e7b3745e9d2869a427b9aab5940e3a95492af5
pascall de creator <pascall.de.creator@gmail.com>
2023-03-16 12:26:53 +0000

    Setup Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif'],
+    },
+    colors: {
+      primary: {
+        50: '#d8f3dc',
+        200: '#b7e4c7',
+        200: '#95d5b2',
+        300: '#74c69d',
+        400: '#52b788',
+        500: '#40916c',
+        600: '#2d6a4f',
+        700: '#1b4332',
+        800: '#173728',
+        800: '#081c15',
+      },
+      secondary: {
+        50: '#f8fafc',
+        100: '#f1f5f9',
+        200: '#e2e8f0',
+        300: '#cbd5e1',
+        400: '#94a3b8',
+        500: '#64748b',
+        600: '#475569',
+        700: '#334155',
+        800: '#1e293b',
+        900: '#0f172a',
+      },
+    },
+  },
+}
