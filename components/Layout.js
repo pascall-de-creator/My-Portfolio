724f74ad7a5fb5fd384e17858feab75515f26e7f
pascall de creator <pascall.de.creator@gmail.com>
2023-03-16 15:38:56 +0000

    Add page Animation

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,40 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
+  const { asPath } = useRouter();
+
+  const variants = {
+    out: {
+      opacity: 0,
+      y: 40,
+      transition: {
+        duration: 0.75
+      }
+    },
+    in: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.75,
+        delay: 0.5
+      }
+    }
+  }
+
   return (
-    <>
-      <Navbar />
-      <main>{ children }</main>
-      <Footer />
-    </>
+    <div className='main-wrapper'>
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.div  key={asPath} variants={variants} animate="in" initial="out" exit="out">
+          <main>
+            { children }
+          </main>
+          <Footer/>
+        </motion.div>
+      </AnimatePresence>
+      <Navbar/>
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 export default function Home() {
   return (
     <>
+      <section className="bg-primary-400 w-screen h-screen">
+
+      </section>
     </>
   )
 }
