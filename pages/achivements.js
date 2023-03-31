a738b2df495378ff0b0608c32d6d2263aed3d9aa
pascall de creator <pascall.de.creator@gmail.com>
2023-03-31 13:40:33 +0100

    add projects and achivements

--- pages/achivements.js
+++ pages/achivements.js
@@ -4,27 +4,25 @@ import Link from "next/link"
 const Works = () => {
     return (
         <section className="p-4 mb-[100px]">
-            <div className='mt-16 flex flex-col items-center gap-4'>
-                <h2>My Works</h2>
-                <div className='w-full flex flex-col sm:grid grid-col-3 grid-flow-col grid-row-2 gap-4'>
-                    <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto sm:row-span-2 rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/vercel.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
+            <div className='mt-16 flex flex-col items-center gap-4 container mx-auto'>
+                <h2>My Achivements</h2>
+                <div className='w-full flex flex-col sm:grid grid-col-3 grid-col-1 gap-4'>
+                    <Link className='project-card bg-secondary-600 aspect-video sm:col-span-2 rounded-lg' href='./'>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/achivements/cert (2).png"></Image>
                     </Link>
                     <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={590} height={332} src="/achivements/cert_01.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/achivements/cert (3).png"></Image>
                     </Link>
-                    
                     <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/visual-json_01.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/achivements/cert (4).png"></Image>
+                    </Link>
+                    <Link className='img-02 project-card bg-secondary-600 sm:row-span-2 rounded-lg' href='./'>
+                    </Link>
+                    <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/achivements/cert (6).png"></Image>
+                    </Link>
+                    <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/achivements/cert (5).png"></Image>
                     </Link>
                 </div>
             </div>
--- pages/works.js
+++ pages/works.js
@@ -4,50 +4,25 @@ import Link from "next/link"
 const Works = () => {
     return (
         <section className="p-4 mb-[100px]">
-            <div className='mt-16 flex flex-col items-center gap-4'>
-                <h2>My Works</h2>
-                <div className='w-full flex flex-col gap-4 sm:grid grid-col-3 grid-row-3 grid-rows-4 grid-flow-col'>
-                    <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto sm:row-span-2 rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/vercel.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
+            <div className='mt-16 flex flex-col items-center gap-4 container mx-auto'>
+                <h2>My Works & contributions</h2>
+                <div className='w-full flex flex-col sm:grid grid-col-3 grid-col-1 gap-4'>
+                    <Link className='project-card bg-secondary-600 aspect-video sm:col-span-2 rounded-lg' href='./'>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/projects/project (2).png"></Image>
                     </Link>
                     <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/visual-json_01.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/projects/project (3).png"></Image>
                     </Link>
                     <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/redwood.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/projects/project (4).png"></Image>
+                    </Link>
+                    <Link className='project-img-02 project-card bg-secondary-600 sm:row-span-2 rounded-lg' href='./'>
                     </Link>
                     <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/blogr.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/projects/project (5).png"></Image>
                     </Link>
                     <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/bot_01.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
-                    </Link>
-                    <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/pdec.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
-                    </Link>
-                    <Link className='project-card bg-secondary-600 aspect-video sm:aspect-auto rounded-lg' href='./'>
-                        <Image className='h-max w-max rounded-lg' alt='test' width={500} height={50} src="/projects/setifi_01.png"></Image>
-                        <div className="p-4 bg-clip-content ">
-                            <h3>Visual Json</h3>
-                        </div>
+                        <Image className='h-max w-max rounded-lg' alt='test' width={1600} height={900} src="/projects/project (6).png"></Image>
                     </Link>
                 </div>
             </div>
